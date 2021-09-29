e77d73005c383b50193576384e688a1a2c7d1b6a
Yunus Emre Alpu <yunus192alpu@gmail.com>
2021-09-29 15:57:54 +0300

    real-time location tracking with socket.io

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ const connectDB = require('./config/db')
 const passport = require('passport')
 const bodyParser = require('body-parser')
 const routes = require('./routes/index')
-
+const http = require("http");
+const server = http.createServer();
+const { Server } = require("socket.io");
 
 connectDB()
 const app = express()
@@ -22,6 +24,46 @@ app.use(routes)
 app.use(passport.initialize())
 require('./config/passport')(passport)
 
+
+let users = [];
+
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:3000",
+    },
+  });
+
+io.on("connection", (socket) => {
+  socket.on("join", (data) => {
+    const user = {
+      socketId: socket.id,
+      coords: data,
+    };
+
+    users.push(user);
+
+    socket.broadcast.emit("new-user", user);
+    socket.emit("current-user", user);
+    socket.emit("users", users);
+  });
+
+  socket.on("position-change", (data) => {
+    users = users.map((u) => {
+      if (u.socketId === data.socketId) {
+        return data;
+      }
+      return u;
+    });
+
+    io.emit("position-change", data);
+    console.log(users);
+  });
+
+  socket.on("disconnect", () => {
+    users = users.filter((u) => u.socketId !== socket.id);
+    socket.broadcast.emit("users", users);
+  });
+});
 
 const PORT = process.env.PORT || 3000
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
